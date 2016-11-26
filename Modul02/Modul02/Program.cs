cdb48b24eb8c0c51d6f23cbc5cac99b60d09e825
Svetlio Slavchev <sslavhev@gmail.comgit config --listgit config --listgit config --listgit config user.emailgit config uesr.name git config --global user.email sslavhev@gmail.com>
2016-11-26 23:46:15 +0200

    Модул 02 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gtk;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// Дефиниция на променливи
+
+			// Събиране на числа
+
+			// Писане в козола
+		}
+	}
+}
