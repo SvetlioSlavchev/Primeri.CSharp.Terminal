b4f64abd856b147a12706e4ad4b434fe12bb44a8
Svetlio Slavchev <sslavhev@gmail.com>
2016-12-01 04:45:48 -0600

    Modul02 - variable type int

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			// Defining variables
+			int a = 0, b = 0;
+			int sum = 0;
+
+			// Collection of variables
+			a = 55;
+			b = 10;
+
+			sum = a + b;
+
+			// Writing in cosole 
+			Console.Write(a);
+			Console.Write(" + ");
+			Console.Write(b);
+			Console.Write(" = ");
+			Console.WriteLine(sum);
+		}
+	}
+}
